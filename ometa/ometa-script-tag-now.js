1088084e4f3b77da87e66dec31bcb3793446d805
Page <pjgazzard@googlemail.com>
2011-06-17 13:46:58 +0100

    Improve parsing error messages that are seen in the console.

--- ometa/ometa-script-tag-now.js
+++ ometa/ometa-script-tag-now.js
@@ -10,9 +10,18 @@ function translateCode(s) {
   var scripts = document.getElementsByTagName("script")
   for (var idx = 0; idx < scripts.length; idx++) {
     var script = scripts[idx]
-    if (script.type === "text/x-ometa-js")
-      eval(translateCode(script.innerHTML))
-	  script.innerHTML = '';
+    if (script.type === "text/x-ometa-js") {
+      var source = script.innerHTML;
+      try {
+        eval(translateCode(source));
+        script.innerHTML = '';
+      }
+      catch(e) {
+        console.log(e);
+        var start = Math.max(0,e.errorPos-20);
+        console.log('Error around: '+source.substring(start, Math.min(source.length,start+40)));
+      }
+    }
   }
 //  if (typeof origOnload === "string")
 //    eval(origOnload)
