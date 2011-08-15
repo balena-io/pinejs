8065c2545e7900629c5c82025412a598130d4773
Page <pjgazzard@googmail.com>
2011-08-15 12:16:44 +0100

    Move ometa-js into HTML5Boilerplate js dir.

--- ometa-dev/rhino/compileAllOmeta.sh
+++ ometa-dev/rhino/compileAllOmeta.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar js.jar -O 9 compile.js ../../ometa-js/bs-js-compiler.txt ../../ometa-js/bs-ometa-compiler.txt ../../ometa-js/bs-ometa-js-compiler.txt ../../ometa-js/bs-ometa-optimizer.txt ../../ometa-js/bs-project-list-parser.txt
+java -jar js.jar -O 9 compile.js ../../js/mylibs/ometa-js/bs-js-compiler.txt ../../js/mylibs/ometa-js/bs-ometa-compiler.txt ../../js/mylibs/ometa-js/bs-ometa-js-compiler.txt ../../js/mylibs/ometa-js/bs-ometa-optimizer.txt ../../js/mylibs/ometa-js/bs-project-list-parser.txt
 read -p "press any key"
