ced37df1c9315d95b10a42f2537e7d3993bc67a4
Page <pjgazzard@googlemail.com>
2011-08-17 20:43:46 +0100

    Integrate the ometa-code with boilerplate

--- ometa-dev/rhino/compileAllSBVR.bat
+++ ometa-dev/rhino/compileAllSBVR.bat
@@ -1,4 +1,4 @@
 %~d0
 cd %~p0
-java -jar js.jar -O 9 compile.js pretty ../../ometa-code/ClientURIParser.ometa ../../ometa-code/ClientURIUnparser.ometa ../../ometa-code/Prettify.ometa ../../ometa-code/SBVR_PreProc.ometa ../../ometa-code/SBVR2SQL.ometa ../../ometa-code/SBVRModels.ometa ../../ometa-code/SBVRParser.ometa ../../ometa-code/ServerURIParser.ometa
+java -jar js.jar -O 9 compile.js pretty ../../js/mylibs/ometa-code/ClientURIParser.ometa ../../js/mylibs/ometa-code/ClientURIUnparser.ometa ../../js/mylibs/ometa-code/Prettify.ometa ../../js/mylibs/ometa-code/SBVR_PreProc.ometa ../../js/mylibs/ometa-code/SBVR2SQL.ometa ../../js/mylibs/ometa-code/SBVRModels.ometa ../../js/mylibs/ometa-code/SBVRParser.ometa ../../js/mylibs/ometa-code/ServerURIParser.ometa
 pause
--- ometa-dev/rhino/compileAllSBVR.sh
+++ ometa-dev/rhino/compileAllSBVR.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar js.jar -O 9 compile.js pretty ../../ometa-code/ClientURIParser.ometa ../../ometa-code/ClientURIUnparser.ometa ../../ometa-code/Prettify.ometa ../../ometa-code/SBVR_PreProc.ometa ../../ometa-code/SBVR2SQL.ometa ../../ometa-code/SBVRModels.ometa ../../ometa-code/SBVRParser.ometa ../../ometa-code/ServerURIParser.ometa
+java -jar js.jar -O 9 compile.js pretty ../../js/mylibs/ometa-code/ClientURIParser.ometa ../../js/mylibs/ometa-code/ClientURIUnparser.ometa ../../js/mylibs/ometa-code/Prettify.ometa ../../js/mylibs/ometa-code/SBVR_PreProc.ometa ../../js/mylibs/ometa-code/SBVR2SQL.ometa ../../js/mylibs/ometa-code/SBVRModels.ometa ../../js/mylibs/ometa-code/SBVRParser.ometa ../../js/mylibs/ometa-code/ServerURIParser.ometa
 read -p "press any key"
