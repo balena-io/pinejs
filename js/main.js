23801d7ceec82728e48d210d3d866411c87e51ab
Page <pjgazzard@googlemail.com>
2011-08-30 14:33:20 +0100

    Add some #IFDEFs for server stuff.

--- js/main.js
+++ js/main.js
@@ -5,22 +5,26 @@ require(["../ometa-js/lib",
 			"mylibs/ometa-code/ClientURIParser",
 			"mylibs/ometa-code/ClientURIUnparser"]);
 	require(["mylibs/inflection",
-			"mylibs/ometa-code/SBVRParser"], function() {
+			"mylibs/ometa-code/SBVRParser",
+			"mylibs/ometa-code/Prettify"], function() {
 		require(["../CodeMirror2/lib/codemirror"], function() {
 			require(["mylibs/cm/sbvr","mylibs/cm/sbvrac"]);
-			require(["../CodeMirror2/mode/plsql/plsql"], function() {
+			/*#IFDEF SERVER */
+			require(["../CodeMirror2/mode/plsql/plsql"], function() {/*#ENDIFDEF*/
 				require(["script"]);
-			})
+			/*#IFDEF SERVER */
+			})/*#ENDIFDEF*/
 		});
 		require(["mylibs/ometa-code/SBVRModels"])
 	});
 	
-	require(["mylibs/json2",
-			"mylibs/ometa-code/SBVRParser",
-			"mylibs/ometa-code/SBVR_PreProc",
-			"mylibs/ometa-code/SBVR2SQL",
-			"mylibs/ometa-code/ServerURIParser",
-			"mylibs/ometa-code/Prettify",
-			"mylibs/server",
-			"mylibs/runTrans"]);
+	/*#IFDEF SERVER */
+		require(["mylibs/json2",
+				"mylibs/ometa-code/SBVRParser",
+				"mylibs/ometa-code/SBVR_PreProc",
+				"mylibs/ometa-code/SBVR2SQL",
+				"mylibs/ometa-code/ServerURIParser",
+				"mylibs/server",
+				"mylibs/runTrans"]);
+	/*#ENDIFDEF*/
 });
