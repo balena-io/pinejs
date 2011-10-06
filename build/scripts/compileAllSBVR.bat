d76e26a0f6db6a48240dd4592139850026e25165
Page <pjgazzard@googmail.com>
2011-10-06 19:21:05 +0100

    Remove a bunch of old files related to the ant build system that we no longer need.

--- build/scripts/compileAllSBVR.bat
+++
@@ -1,4 +0,0 @@
-%~d0
-cd %~p0..
-java -jar tools/rhino.jar -O 9 tools/ometac.js ../ometa-js pretty ../../js/mylibs/ometa-code/ClientURIParser.ometa ../../js/mylibs/ometa-code/ClientURIUnparser.ometa ../../js/mylibs/ometa-code/Prettify.ometa ../../js/mylibs/ometa-code/SBVR_PreProc.ometa ../../js/mylibs/ometa-code/SBVR2SQL.ometa ../../js/mylibs/ometa-code/SBVRModels.ometa ../../js/mylibs/ometa-code/SBVRParser.ometa ../../js/mylibs/ometa-code/ServerURIParser.ometa
-pause
