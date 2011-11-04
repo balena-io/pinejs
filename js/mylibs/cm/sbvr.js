0bdd89f4d77c286b75fcd6bd126c91695ea8f403
Page <pjgazzard@googmail.com>
2011-11-04 13:28:15 +0000

    We do need to copy the state in more places.

--- js/mylibs/cm/sbvr.js
+++ js/mylibs/cm/sbvr.js
@@ -2,14 +2,7 @@ CodeMirror.defineMode("sbvr", function(config) {
 
 	return {
 		copyState: function(state) {
-			//We only copy state at the end of the line, this reduces the number of copy calls we need to make.
-			if(state._codemirrorEOL === true) {
-				state._codemirrorEOL = false;
-				return $.extend(true,{},state);
-			}
-			else {
-				return state;
-			}
+			return $.extend(true,{},state);
 		},
 	
 		startState: function(base) {
@@ -26,49 +19,43 @@ CodeMirror.defineMode("sbvr", function(config) {
 		},
 
 		token: function(stream, state) {
-			var returnVal = function(stream, state) {
-				if(stream.sol()) { //Reset most of the state because it's a new line.
-					try {
-						this.blankLine(state);
-						state.matchAll(stream.string,'line');
-					}
-					catch(e) {}
+			if(stream.sol()) { //Reset most of the state because it's a new line.
+				try {
+					this.blankLine(state);
+					state.matchAll(stream.string,'line');
 				}
-				if(state.nextToken != undefined && state.nextToken != null) {
-					var nextToken = state.nextToken;
-					state.nextToken = null;
-					stream.pos = nextToken[0];
-					return "sbvr-"+nextToken[1];
-				}
-				if(state._tokens[stream.pos]) {
-					var currTokens = state._tokens[stream.pos];
-					delete state._tokens[stream.pos];
-					for (var i in currTokens) {
-						if(state.keyTokens.indexOf(currTokens[i][1])!=-1) {
-							if(stream.eatSpace()) {
-								state.nextToken = currTokens[i];
-								return null;
-							}
-							stream.pos = currTokens[i][0];
-							return "sbvr-"+currTokens[i][1];
-						}
-					}
-				}
-				for(var i in state._tokens) {
-					if(isNaN(parseInt(i))) {
-						stream.skipToEnd();
-						return null;
-					}
-					if(i>stream.pos) {
-						stream.pos = i;
-						return null;
-					}
-				}
-			}.call(this, stream, state);
-			if(stream.eol()) {
-				state._codemirrorEOL = true;
+				catch(e) {}
 			}
-			return returnVal;
+			if(state.nextToken != undefined && state.nextToken != null) {
+				var nextToken = state.nextToken;
+				state.nextToken = null;
+				stream.pos = nextToken[0];
+				return "sbvr-"+nextToken[1];
+			}
+			if(state._tokens[stream.pos]) {
+				var currTokens = state._tokens[stream.pos];
+				delete state._tokens[stream.pos];
+				for (var i in currTokens) {
+					if(state.keyTokens.indexOf(currTokens[i][1])!=-1) {
+						if(stream.eatSpace()) {
+							state.nextToken = currTokens[i];
+							return null;
+						}
+						stream.pos = currTokens[i][0];
+						return "sbvr-"+currTokens[i][1];
+					}
+				}
+			}
+			for(var i in state._tokens) {
+				if(isNaN(parseInt(i))) {
+					stream.skipToEnd();
+					return null;
+				}
+				if(i>stream.pos) {
+					stream.pos = i;
+					return null;
+				}
+			}
 		},
 
 		indent: function(state, textAfter) {
