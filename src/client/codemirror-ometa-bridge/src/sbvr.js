35c11daab1d6b38c55ad9b9a0569f9c98f77710c
Page <pjgazzard@googmail.com>
2012-02-07 17:28:15 +0000

    Make sbvr.js a module.

--- src/client/codemirror-ometa-bridge/src/sbvr.js
+++ src/client/codemirror-ometa-bridge/src/sbvr.js
@@ -1,68 +1,70 @@
-CodeMirror.defineMode("sbvr", function(config) {
+define(['SBVRParser', 'codemirror'], function(SBVRParser) {
+	CodeMirror.defineMode("sbvr", function(config) {
 
-	return {
-		copyState: function(state) {
-			return $.extend(true,{},state);
-		},
-	
-		startState: function(base) {
-			return SBVRParser.createInstance();
-		},
+		return {
+			copyState: function(state) {
+				return $.extend(true,{},state);
+			},
 		
-		compareStates: function(origState, newState) {
-			return origState.equals(newState);
-		},
-		
-		blankLine: function(state) {
-			state._tokens = [];
-			state.__possibilities = [];
-		},
+			startState: function(base) {
+				return SBVRParser.createInstance();
+			},
+			
+			compareStates: function(origState, newState) {
+				return origState.equals(newState);
+			},
+			
+			blankLine: function(state) {
+				state._tokens = [];
+				state.__possibilities = [];
+			},
 
-		token: function(stream, state) {
-			if(stream.sol()) { //Reset most of the state because it's a new line.
-				try {
-					this.blankLine(state);
-					state.matchAll(stream.string,'line');
+			token: function(stream, state) {
+				if(stream.sol()) { //Reset most of the state because it's a new line.
+					try {
+						this.blankLine(state);
+						state.matchAll(stream.string,'line');
+					}
+					catch(e) {}
 				}
-				catch(e) {}
-			}
-			if(state.nextToken != undefined && state.nextToken != null) {
-				var nextToken = state.nextToken;
-				state.nextToken = null;
-				stream.pos = nextToken[0];
-				return "sbvr-"+nextToken[1];
-			}
-			if(state._tokens[stream.pos]) {
-				var currTokens = state._tokens[stream.pos];
-				delete state._tokens[stream.pos];
-				for (var i in currTokens) {
-					if(state.keyTokens.indexOf(currTokens[i][1])!=-1) {
-						if(stream.eatSpace()) {
-							state.nextToken = currTokens[i];
-							return null;
+				if(state.nextToken != undefined && state.nextToken != null) {
+					var nextToken = state.nextToken;
+					state.nextToken = null;
+					stream.pos = nextToken[0];
+					return "sbvr-"+nextToken[1];
+				}
+				if(state._tokens[stream.pos]) {
+					var currTokens = state._tokens[stream.pos];
+					delete state._tokens[stream.pos];
+					for (var i in currTokens) {
+						if(state.keyTokens.indexOf(currTokens[i][1])!=-1) {
+							if(stream.eatSpace()) {
+								state.nextToken = currTokens[i];
+								return null;
+							}
+							stream.pos = currTokens[i][0];
+							return "sbvr-"+currTokens[i][1];
 						}
-						stream.pos = currTokens[i][0];
-						return "sbvr-"+currTokens[i][1];
 					}
 				}
-			}
-			for(var i in state._tokens) {
-				if(isNaN(parseInt(i))) {
-					stream.skipToEnd();
-					return null;
+				for(var i in state._tokens) {
+					if(isNaN(parseInt(i))) {
+						stream.skipToEnd();
+						return null;
+					}
+					if(i>stream.pos) {
+						stream.pos = i;
+						return null;
+					}
 				}
-				if(i>stream.pos) {
-					stream.pos = i;
-					return null;
-				}
+			},
+
+			indent: function(state, textAfter) {
+				return 0; //We don't indent SBVR
 			}
-		},
+			
+		};
+	});
 
-		indent: function(state, textAfter) {
-			return 0; //We don't indent SBVR
-		}
-		
-	};
-});
-
-CodeMirror.defineMIME("text/sbvr", "sbvr");
+	CodeMirror.defineMIME("text/sbvr", "sbvr");
+}
