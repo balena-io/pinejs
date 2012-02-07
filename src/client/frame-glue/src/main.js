b6758cea59a431fe1c6a03f2d3978e45b2661893
Page <pjgazzard@googmail.com>
2012-02-07 16:54:13 +0000

    Update paths in main.js

--- src/client/frame-glue/src/main.js
+++ src/client/frame-glue/src/main.js
@@ -2,23 +2,22 @@
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
 {console.log();return window.console;}catch(err){return window.console={};}})());
 
-require(["../ometa-js/lib",
-		"../ometa-js/ometa-base"], function() {
+require(["ometa-base"], function() {
 	require(["libs/json2",
 			/*#IFDEF server */
-				"mylibs/drawDataUI",
-				"mylibs/runTrans",
+				"data-frame/drawDataUI",
+				"data-frame/runTrans",
 			/*#ENDIFDEF*/
-			"mylibs/ometa-code/ClientURIParser",
-			"mylibs/ometa-code/ClientURIUnparser"]);
-	require(["libs/inflection",
-			"mylibs/ometa-code/SBVRParser",
+			"data-frame/ClientURIParser",
+			"data-frame/ClientURIUnparser"]);
+	require(["inflection",
+			"SBVRParser",
 			"mylibs/ometa-code/Prettify"], function() {
-		require(["../CodeMirror2/lib/codemirror"], function() {
+		require(["codemirror"], function() {
 			require(["../CodeMirror2/lib/util/simple-hint"], function() {
 				require(["mylibs/cm/sbvr","mylibs/cm/sbvrac"]);
 				/*#IFDEF server */
-				require(["../CodeMirror2/mode/plsql/plsql"], function() {/*#ENDIFDEF*/
+				require(["codemirror-modes/plsql/plsql"], function() {/*#ENDIFDEF*/
 					require(["script"]);
 				/*#IFDEF server */
 				})/*#ENDIFDEF*/
