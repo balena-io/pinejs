79daf9d9784607ca6fc4ea03cd3d1b2c0f99b3e3
alexandrosM <al3xgr@gmail.com>
2012-02-21 21:29:29 +0000

    Complete test case. Needs improvement (particularly in not waiting for timeout and using find_and_click more)

--- test/canvas-demo/test-demo.py
+++ test/canvas-demo/test-demo.py
@@ -3,6 +3,18 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time, sys, os
 
+def find_and_click(where, why):
+	try:
+		#print(where, why)
+		elem = browser.find_element_by_xpath(where)
+		#elem.click() # this should work except for a selenium/chrome bug. http://code.google.com/p/selenium/issues/detail?id=2766
+		#So we are forced to directly navigate to the link below.
+		#print elem.get_attribute('href')
+		browser.get(elem.get_attribute('href'))
+	except NoSuchElementException:
+		assert 0, "can't %s" % why
+	time.sleep(1)
+
 #TODO: Reduce time by waiting for next event specifically instead of adding random sleep time.
 
 browser = webdriver.Chrome()
@@ -13,50 +25,64 @@ root_path = '/'.join(root_path)
 
 browser.get("file:///%s/src/client/frame-glue/src/index.html" % root_path)
 time.sleep(1)
+
 browser.find_element_by_id("blm1").click()
+time.sleep(1)
+
 browser.find_element_by_id("bem").click()
 time.sleep(1)
 
 try:
-    browser.find_element_by_xpath("//a[contains(@href,'#importExportTab')]").click()
+	browser.find_element_by_xpath("//a[contains(@href,'#importExportTab')]").click()
 except NoSuchElementException:
-    assert 0, "can't find importExportTab"
+	assert 0, "can't switch to importExportTab"
 
 #This string has double escaped double quotes because the first slash is consumed by 
 #the python string, and then the second slash is used to differentiate the double quotes 
 #within the string (escaped) from those enclosing the string (not escaped). Also, it's 
-#all one line as newlines make the javascript fail. Similar principles for the '\\n'.
+#all one line as newlines make the javascript fail. Same goes for the '\\n'.
 
 SQLdata = '''INSERT INTO \\"pilot\\" (\\"id\\",\\"name\\") values ('1','Joachim');\\nINSERT INTO \\"pilot\\" (\\"id\\",\\"name\\") values ('2','Esteban');\\nINSERT INTO \\"plane\\" (\\"id\\",\\"name\\") values ('1','Boeing 747');\\nINSERT INTO \\"plane\\" (\\"id\\",\\"name\\") values ('2','Spitfire');\\nINSERT INTO \\"plane\\" (\\"id\\",\\"name\\") values ('3','Concorde');\\nINSERT INTO \\"plane\\" (\\"id\\",\\"name\\") values ('4','Mirage 2000');\\nINSERT INTO \\"pilot-can_fly-plane\\" (\\"id\\",\\"pilot_id\\",\\"plane_id\\") values ('1','1','2');\\nINSERT INTO \\"pilot-can_fly-plane\\" (\\"id\\",\\"pilot_id\\",\\"plane_id\\") values ('2','1','3');\\nINSERT INTO \\"pilot-can_fly-plane\\" (\\"id\\",\\"pilot_id\\",\\"plane_id\\") values ('3','1','4');\\nINSERT INTO \\"pilot-can_fly-plane\\" (\\"id\\",\\"pilot_id\\",\\"plane_id\\") values ('4','2','1');\\nINSERT INTO \\"pilot-is_experienced\\" (\\"id\\",\\"pilot_id\\") values ('1','1');'''
 
 browser.execute_script('window.importExportEditor.setValue("%s");' % SQLdata)
 browser.find_element_by_id("bidb").click()
+time.sleep(1)
 
 try:
-    browser.find_element_by_xpath("//a[contains(@href,'#dataTab')]").click()
+	browser.find_element_by_xpath("//a[contains(@href,'#dataTab')]").click()
 except NoSuchElementException:
-    assert 0, "can't find dataTab"
+	assert 0, "can't switch to dataTab"
+time.sleep(1)
 
-'''here are the 11 remaining steps for this test case:'''
-	
-'''expand pilots'''
+#here are the 11 remaining steps for this test case:
 
-'''expand pilot can fly plane'''
+find_and_click("//a[contains(@onclick,'pilot\"')]", "expand pilots")
 
-'''delete joachim can fly _some plane_'''
+find_and_click("//a[contains(@onclick,'pilot/pilot-can_fly-plane\"')]", "can't expand pilot can fly plane")
+
+find_and_click("//a[contains(@onclick,'pilot/pilot-can_fly-plane/pilot-can_fly-plane.3*del')]", "delete fact [Joachim can fly Boeing 747]")
 
 '''click -confirm- or whatever'''
-
-'''check if error'''
+browser.find_element_by_xpath("//tr[@id='tr--data--pilot-can_fly-plane']//input[@type='submit']").click()
+time.sleep(1)
 
 '''click revise request'''
+browser.find_element_by_xpath("//button[descendant::text()='Revise Request']").click()
+time.sleep(1)
 
 '''expand pilot is experienced'''
+find_and_click("//a[contains(@onclick,'pilot/(pilot-can_fly-plane/pilot-can_fly-plane.3*del,pilot-is_experienced)\"')]", "expand pilot is experienced")
 
 '''delete joachim is experienced'''
+find_and_click("//a[contains(@onclick,'/pilot/(pilot-can_fly-plane/pilot-can_fly-plane.3*del,pilot-is_experienced/pilot-is_experienced.1*del)\"')]", "delete fact [Joachim is experienced]")
 
 '''click Apply All'''
+browser.find_element_by_xpath("//input[@value='Apply All Changes']").click()
+time.sleep(1)
 
-'''check if error (how?)'''
+'''check if error'''
+if not browser.current_url.split('#')[-1] == '!/data/':
+	assert 0, "Transaction not completed successfully"
 
 '''close browser'''
+browser.close()
