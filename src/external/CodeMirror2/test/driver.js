7b6a36ac6fc22de450c1bfe4be3c01fc3dc78213
Page <pjgazzard@googlemail.com>
2012-09-11 17:44:16 +0100

    Update CodeMirror.

--- src/external/CodeMirror2/test/driver.js
+++ src/external/CodeMirror2/test/driver.js
@@ -1,35 +1,111 @@
-var tests = [], debug = null;
+var tests = [], debug = null, debugUsed = new Array(), allNames = [];
 
 function Failure(why) {this.message = why;}
 
+function indexOf(collection, elt) {
+  if (collection.indexOf) return collection.indexOf(elt);
+  for (var i = 0, e = collection.length; i < e; ++i)
+    if (collection[i] == elt) return i;
+  return -1;
+}
+
 function test(name, run, expectedFail) {
+  // Force unique names
+  var originalName = name;
+  var i = 2; // Second function would be NAME_2
+  while (indexOf(allNames, name) !== -1){
+    name = originalName + "_" + i;
+    i++;
+  }
+  allNames.push(name);
+  // Add test
   tests.push({name: name, func: run, expectedFail: expectedFail});
   return name;
 }
 function testCM(name, run, opts, expectedFail) {
-  return test(name, function() {
+  return test("core_" + name, function() {
     var place = document.getElementById("testground"), cm = CodeMirror(place, opts);
-    if (debug) place.style.visibility = "";
-    try {run(cm);}
-    finally {if (!debug) place.removeChild(cm.getWrapperElement());}
+    var successful = false;
+    try {
+      run(cm);
+      successful = true;
+    } finally {
+      if ((debug && !successful) || verbose) {
+        place.style.visibility = "";
+      } else {
+        place.removeChild(cm.getWrapperElement());
+      }
+    }
   }, expectedFail);
 }
 
 function runTests(callback) {
+  if (debug) {
+    if (indexOf(debug, "verbose") === 0) {
+      verbose = true;
+      debug.splice(0, 1);
+    }
+    if (debug.length < 1) {
+      debug = null;
+    } else {
+      if (totalTests > debug.length) {
+        totalTests = debug.length;
+      }
+    }
+  }
+  var totalTime = 0;
   function step(i) {
-    if (i == tests.length) return callback("done");
-    var test = tests[i], expFail = test.expectedFail;
-    if (debug != null && debug != test.name) return step(i + 1);
+    if (i === tests.length){
+      running = false;
+      return callback("done");
+    }
+    var test = tests[i], expFail = test.expectedFail, startTime = +new Date;
+    if (debug !== null) {
+      var debugIndex = indexOf(debug, test.name);
+      if (debugIndex !== -1) {
+        // Remove from array for reporting incorrect tests later
+        debug.splice(debugIndex, 1);
+      } else {
+        var wildcardName = test.name.split("_").shift() + "_*";
+        debugIndex = indexOf(debug, wildcardName);
+        if (debugIndex !== -1) {
+          // Remove from array for reporting incorrect tests later
+          debug.splice(debugIndex, 1);
+          debugUsed.push(wildcardName);
+        } else {
+          debugIndex = indexOf(debugUsed, wildcardName);
+          if (debugIndex !== -1) {
+            totalTests++;
+          } else {
+            return step(i + 1);
+          }
+        }
+      }
+    }
     try {
-      test.func();
-      if (expFail) callback("fail", test.name, "was expected to fail, but succeeded");
-      else callback("ok", test.name);
+      var message = test.func();
+      if (expFail) callback("fail", test.name, message);
+      else callback("ok", test.name, message);
     } catch(e) {
       if (expFail) callback("expected", test.name);
       else if (e instanceof Failure) callback("fail", test.name, e.message);
-      else callback("error", test.name, e.toString());
+      else {
+        var pos = /\bat .*?([^\/:]+):(\d+):/.exec(e.stack);
+        callback("error", test.name, e.toString() + (pos ? " (" + pos[1] + ":" + pos[2] + ")" : ""));
+      }
     }
-    setTimeout(function(){step(i + 1);}, 20);
+    if (!quit) { // Run next test
+      var delay = 0;
+      totalTime += (+new Date) - startTime;
+      if (totalTime > 500){
+        totalTime = 0;
+        delay = 50;
+      }
+      setTimeout(function(){step(i + 1);}, delay);
+    } else { // Quit tests
+      running = false;
+      return null;
+    }
   }
   step(0);
 }
