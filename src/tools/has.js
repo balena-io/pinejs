ce1340e04bc14d6e5f054a2dfba0a87432aa9d9b
Petros Aggelatos <petrosagg@gmail.com>
2012-11-20 14:36:49 +0200

    Correct implementation of has.js

--- src/tools/has.js
+++ src/tools/has.js
@@ -1,6 +1,6 @@
 define(['module'], function (module) {
 	var config = module.config();
 	return function (flag) {
-		return config.hasOwnProperty(flag);
+		return config.hasOwnProperty(flag) && config[flag];
 	};
 });
