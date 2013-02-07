8f578069c4b7a334684892ef5edc84d45546f79e
Page <pjgazzard@googlemail.com>
2013-02-07 15:29:59 +0000

    Fix client build config.

--- src/client/build/client.build.js
+++ src/client/build/client.build.js
@@ -1,14 +1,14 @@
 ({
-    appDir: '../src',
-    dir: '../out',
-    stubModules: ['cs', 'text', 'ometa', 'css'],
-    mainConfigFile: '../out/main.js',
-    optimize: 'uglify',
-    separateCSS: true,
-    modules: [
-    	{
+	appDir: '../src',
+	dir: '../out',
+	stubModules: ['cs', 'text', 'ometa', 'css'],
+	mainConfigFile: '../src/main.js',
+	optimize: 'uglify',
+	separateCSS: true,
+	modules: [
+		{
 			name: "main",
 			exclude: ['coffee-script']
 		}
-    ]
+	]
 })
