f9d61b71bfb35137c09fd93c03c2aa8273b1e0c4
Page <pjgazzard@googlemail.com>
2013-02-26 20:37:47 +0000

    Add a version identifier and has config listing to built versions of the platform.

--- src/server/build/browser.build.js
+++ src/server/build/browser.build.js
@@ -1,5 +1,35 @@
 ({
-	out: 'platform.js',
+	out: function(text) {
+		var buildDir = this.baseUrl + '../build',
+			hasConfig = JSON.stringify(this.has, null, '\t'),
+			childProcess = nodeRequire('child_process');
+		childProcess.exec('git describe --tags', {
+			cwd: buildDir
+		}, function(err, stdout, stderr) {
+			if(err) {
+				console.error(err);
+				throw err;
+			}
+			childProcess.exec('git diff --exit-code', {
+				cwd: buildDir
+			}, function(err) {
+				var workingDirChangeSignifier = '';
+				if(err) {
+					// There are working dir changes.
+					workingDirChangeSignifier = '+';
+				}
+				text = '// Build: ' + stdout.trim() + workingDirChangeSignifier + '\n' +
+						'/* has: ' + hasConfig + ' */\n' +
+					text;
+				nodeRequire('fs').writeFile(buildDir + '/platform.js', text, function(err) {
+					if(err) {
+						console.error(err);
+						throw err;
+					}
+				});
+			});
+		});
+	},
 
 	stubModules: [
 		'text',
@@ -12,6 +42,7 @@
 		'css/normalize', 'css/css',
 		'ometa-compiler', 'uglifyjs'
 	],
+	preserveLicenseComments: false,
 	mainConfigFile: '../src/main.js',
 
 	optimize: 'uglify2',
