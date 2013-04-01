a7b2d5e4a3351dd1e7a82577e38c7fdba178e9ff
Page <pjgazzard@googlemail.com>
2013-04-01 18:23:50 +0100

    Add paths for the odata parsers so they will be included in the build.

--- src/server/src/main.js
+++ src/server/src/main.js
@@ -14,23 +14,25 @@ require({
 	},
 	paths: {
 		//Developing & building tools
-		'cache'				:  '../../tools/requirejs-plugins/cache',
-		'cs'				:  '../../tools/requirejs-plugins/cs',
-		'ometa'				:  '../../../node_modules/ometa-js/lib/requirejs-plugin/ometajs',
-		'text'				:  '../../tools/requirejs-plugins/text',
-		'coffee-script'		:  '../../tools/coffee-script',
-		'has'				:  '../../tools/has',
+		'cache'					: '../../tools/requirejs-plugins/cache',
+		'cs'					: '../../tools/requirejs-plugins/cs',
+		'ometa'					: '../../../node_modules/ometa-js/lib/requirejs-plugin/ometajs',
+		'text'					: '../../tools/requirejs-plugins/text',
+		'coffee-script'			: '../../tools/coffee-script',
+		'has'					: '../../tools/has',
 
-		'ometa-parsers'	:  '../../../node_modules/ometa-js/lib/ometajs/ometa/parsers',
-		'uglifyjs'			: '../../external/uglifyjs/uglify',
+		'ometa-parsers'			: '../../../node_modules/ometa-js/lib/ometajs/ometa/parsers',
+		'uglifyjs'				: '../../external/uglifyjs/uglify',
 
 		//Libraries
-		'ometa-core'		:  '../../../node_modules/ometa-js/lib/ometajs/core',
-		'OMeta'				:  '../../../node_modules/ometa-js/lib/ometajs/core',
-		'sbvr-parser'		:  '../../common/sbvr-parser',
-		'utils'				:  '../../common/utils',
-		'prettify'			:  '../../common/prettify',
-		'inflection'		:  '../../external/inflection/inflection',
+		'ometa-core'			: '../../../node_modules/ometa-js/lib/ometajs/core',
+		'OMeta'					: '../../../node_modules/ometa-js/lib/ometajs/core',
+		'sbvr-parser'			: '../../common/sbvr-parser',
+		'utils'					: '../../common/utils',
+		'prettify'				: '../../common/prettify',
+		'inflection'			: '../../external/inflection/inflection',
+		'odata-parser'			: '../../../node_modules/odata-parser/odata-parser',
+		'odata-to-abstract-sql'	: '../../../node_modules/odata-to-abstract-sql/odata-to-abstract-sql'
 	},
 	shim: {
 		'uglifyjs': {
