2953736d01bbaf31b32dba20be2599b39e4fb280
Page <pjgazzard@googlemail.com>
2013-06-16 20:36:06 +0100

    Change the dependency installation script to run in serial rather than parallel to avoid issues with npm locks.

--- install.js
+++ install.js
@@ -1,31 +1,45 @@
+// This install script is used to work around the issue that the dependencies of a private git repo do not get installed on an npm install, so it makes sure they are installed before trying to build.
+
 var dependencies = require('./package.json').dependencies,
 	childProcess = require('child_process'),
-	dependenciesRemaining = Object.keys(dependencies).length,
+	dependenciesKeys = Object.keys(dependencies),
 	windows = require('os').platform() == 'win32';
-for(dependency in dependencies) {
+
+checkDependency(0);
+
+function checkDependency(i) {
+	if(i === dependenciesKeys.length) {
+		return cleanup();
+	}
+	var dependency = dependenciesKeys[i];
 	// Workaround for bcrypt being insanely hard to get building on windows.
 	if(windows && dependency == 'bcrypt') {
-		dependenciesRemaining--;
-		continue;
+		return checkDependency(i+1);
 	}
-	var command = 'npm install ' + dependency + '@' + dependencies[dependency];
+	try {
+		require(dependency);
+		checkDependency(i+1);
+	} catch (e) {
+		var command = 'npm install ' + dependency + '@' + dependencies[dependency];
+		console.log(command);
+		childProcess.exec(command, {}, function(err, stdout, stderr) {
+			if(err) {
+				console.error(err);
+				process.exit(1);
+			}
+			checkDependency(i+1);
+		});
+	}
+}
+
+function cleanup() {
+	var command = 'node node_modules/requirejs/bin/r.js -o src/server/build/server.build.js out=platform.js';
 	console.log(command);
 	childProcess.exec(command, {}, function(err, stdout, stderr) {
 		if(err) {
 			console.error(err);
 			process.exit(1);
 		}
-		dependenciesRemaining--;
-		if(dependenciesRemaining == 0) {
-			var command = 'node node_modules/requirejs/bin/r.js -o src/server/build/server.build.js out=platform.js';
-			console.log(command);
-			childProcess.exec(command, {}, function(err, stdout, stderr) {
-				if(err) {
-					console.error(err);
-					process.exit(1);
-				}
-				process.exit(0);
-			});
-		}
+		process.exit(0);
 	});
 }
