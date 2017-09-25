ff369b8866c6b9980649a06880c4873bc2f1eba8
Pagan Gazzard <page@resin.io>
2017-09-25 13:00:41 +0100

    Convert extended-sbvr-parser to typescript
    Change-type: patch

---
+++ src/extended-sbvr-parser/extended-sbvr-parser.ts
@@ -0,0 +1,12 @@
+const { SBVRParser } = require('@resin/sbvr-parser')
+const Types = require('@resin/sbvr-types/Type.sbvr')
+
+module.exports = SBVRParser._extend({
+	initialize: () => {
+		SBVRParser.initialize.call(this)
+		this.AddCustomAttribute('Database ID Field:')
+		this.AddCustomAttribute('Database Table Name:')
+		this.AddBuiltInVocab(Types)
+		return this
+	}
+})
