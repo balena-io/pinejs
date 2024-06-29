3d932eebe78d37d9db0bf58708e018c89fa0e123
Otavio Jacobi <otaviojacobi@gmail.com>
2024-06-28 22:19:49 -0300

    Add tests for resource renames with multiple translation levels
    This covers errors where resource renames only worked with a single
    translation level due to aliases being included and breaking the
    subsequent levels

---
+++ test/fixtures/04-translations/translations/v5/index.ts
@@ -0,0 +1,18 @@
+import { getAbstractSqlModelFromFile } from '../../../../../src/bin/utils';
+import type { AbstractSqlQuery } from '@balena/abstract-sql-compiler';
+
+export const v5AbstractSqlModel = getAbstractSqlModelFromFile(
+	__dirname + '/university.sbvr',
+	undefined,
+);
+
+export const toVersion = 'university';
+
+v5AbstractSqlModel.tables['student'].fields.push({
+	fieldName: 'computed field',
+	dataType: 'Text',
+	required: false,
+	computed: ['EmbeddedText', 'v5_computed_field'] as AbstractSqlQuery,
+});
+
+v5AbstractSqlModel.relationships['version'] = { v5: {} };
